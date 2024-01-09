42ac1badc051d6b6f571bd0bfbdf17984ec7248b
jianghc <jianghc@yillionbank.com>
2024-01-09 15:01:16 +0800

    添加新功能

--- DataHistory.xaml.cs
+++ DataHistory.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using irm_wpf.Entity;
 
 namespace IRM
 {
@@ -22,9 +23,28 @@ namespace IRM
     /// </summary>
     public partial class DataHistory : Window
     {   
+        public ICommand ViewCommand { get; private set; }
+
+        public ICommand DelCommand { get; private set; }
+
         public DataHistory()
         {
             InitializeComponent();
+
+            ViewCommand = new RelayCommand<DataList>(ViewCommandExecute);
+
+            DelCommand = new RelayCommand<DataList>(DelCommandExecute);
+
+        }
+
+        private void ViewCommandExecute(DataList parameter)
+        {
+            // 操作查看
+        }
+
+        private void DelCommandExecute(DataList parameter)
+        {
+            // 操作删除
         }
     }
 }
